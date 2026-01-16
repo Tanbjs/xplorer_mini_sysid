63a0442f8c8c407b64d5a4202b85917837a5df8c
tanbjs <benjaset.m@gmail.com>
2026-01-17 00:16:32 +0700

    feat: add signal type enum with helper function

--- include/xplorer_mini_sysid/signal_generate_lib.hpp
+++ include/xplorer_mini_sysid/signal_generate_lib.hpp
@@ -8,36 +8,63 @@
 
 namespace SysID 
 {
-    namespace SignalGen 
+    namespace SignalGenerator
     {
-        Eigen::MatrixXd PRBS(
-            const int &n_samples,
-            const int &n_signals,
-            const int &min_switch_time,
-            const int &max_switch_time,
-            const Eigen::VectorXd &amplitudes
-        );
+        enum class SignalType 
+        {
+            RBS,
+            RGS,
+            CHIRP,
+            PRBS,
+            MULTISINE,
+            UNKNOWN
+        };
 
-        Eigen::MatrixXd UniformWhiteNoise(
-            const int &n_samples,
-            const int &n_signals,
-            const Eigen::VectorXd &amplitudes
-        );
+        // Helper function to convert string to enum
+        inline SignalType stringToSignalType(const std::string& str) 
+        {
+            if (str == "RBS") return SignalType::RBS;
+            if (str == "RGS") return SignalType::RGS;
+            if (str == "Chirp") return SignalType::CHIRP;
+            if (str == "PRBS") return SignalType::PRBS;
+            if (str == "Multisine") return SignalType::MULTISINE;
+            return SignalType::UNKNOWN;
+        }
 
-        Eigen::MatrixXd Multisine(
-            const int &n_samples,
-            const int &n_signals,
-            const int &f_max,
-            const Eigen::VectorXd &amplitudes
-        );
+        struct RBSConfig
+        {
+            Eigen::Vector<double, 6> max = Eigen::VectorXd::Zero(6);
+            Eigen::Vector<double, 6> min = Eigen::VectorXd::Zero(6);
+        };
 
-        Eigen::MatrixXd Chirp(
-            const int &n_samples,
-            const int &n_signals,
-            const double &f_start,
-            const double &f_end,
-            const Eigen::VectorXd &amplitudes
-        );
+        struct RGSConfig
+        {
+            Eigen::Vector<double, 6> mean = Eigen::VectorXd::Zero(6);
+            Eigen::Vector<double, 6> stddev = Eigen::VectorXd::Zero(6);
+        };
+
+        struct MultisineConfig
+        {
+            int n_sines = 0;            // Number of sinusoids
+            int grid_skips = 0;         // Number of frequency grid skips
+            int n_trails = 0;           // Number of trails 
+            Eigen::Vector<double, 6> amplitudes = Eigen::VectorXd::Zero(6);
+        };
+
+        struct ChirpConfig
+        {
+        };
+
+        struct PRBSConfig
+        {
+        };
+
+        Eigen::MatrixXd RBS(const int &n_samples, const int &n_signals, const RBSConfig &config);
+        Eigen::MatrixXd RGS(const int &n_samples, const int &n_signals, const RGSConfig &config);
+        Eigen::MatrixXd Multisine(const int &n_samples, const int &n_signals, const MultisineConfig &config);
+        Eigen::MatrixXd Chirp(const int &n_samples, const int &n_signals, const ChirpConfig &config);
+        Eigen::MatrixXd PRBS(const int &n_samples, const int &n_signals, const PRBSConfig &config);
+
     };
 }
 #endif // __SIGNAL_GEN_LIB_HPP__
