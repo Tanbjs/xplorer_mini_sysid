ebe9476fe471370cdcc9b32cdaff07e3b3953ebb
tanbjs <benjaset.m@gmail.com>
2026-04-09 00:14:36 +0700

    feat: add config to pull mode with specific version

--- launch/xplorer_mini_km_monitor.launch.py
+++ launch/xplorer_mini_km_monitor.launch.py
@@ -5,7 +5,13 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
-        
+    
+    koopman_monitor_config = os.path.join(
+        get_package_share_directory('xplorer_mini_sysid'),
+        'config',
+        'koopman_monitor_config.yaml'
+    )
+
     monitor_node = Node(
         package='xplorer_mini_sysid',
         name='km_monitor',
@@ -13,6 +19,7 @@ def generate_launch_description():
         executable='km_monitor.py',
         output="screen",
         emulate_tty=True,
+        parameters=[koopman_monitor_config, {'use_sim_time': True}]
     )
 
     ld.add_action(monitor_node)
