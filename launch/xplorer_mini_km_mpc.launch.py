650414d4feb67664d69a8a3e1f59d951c2ea9e4b
tanbjs <benjaset.m@gmail.com>
2026-04-21 09:46:34 +0700

    feat: edit launch with new code

--- launch/xplorer_mini_km_mpc.launch.py
+++ launch/xplorer_mini_km_mpc.launch.py
@@ -6,53 +6,37 @@ from launch.event_handlers import (OnProcessStart)
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
+    
+    model = 'edmdc'                 # 'dmdc' or 'edmdc'
+    pose_controller = 'ffpi'        # 'ffpi' or 'pi'
+    vel_controller = 'intmpc'       # 'pid' or 'kmpc' (model-based MPC)
+    constrained = True              # Whether to use constrained MPC (only for kmpc)
+    use_preview = True              # Whether to use preview control (only for kmpc)
+
 
     auv_param = os.path.join(
         get_package_share_directory('xplorer_mini_descriptions'),
-            'robots',
-            'xplorer_mini_dynamic_parameters.yaml'
-        )
-    
+        'robots',
+        'xplorer_mini_dynamic_parameters.yaml'
+    )
+
     dpid_gain = os.path.join(
         get_package_share_directory('xplorer_mini_sysid'),
         'config',
         'controller',
         'dpid_gain.yaml'
     )  
-
-    ff_pi_pid_gain = os.path.join(
-        get_package_share_directory('xplorer_mini_sysid'),
-        'config',
-        'controller',
-        'ff_pi_pid_gain.yaml'
-    )
-
-    pid_kmpc_gain = os.path.join(
-        get_package_share_directory('xplorer_mini_sysid'),
-        'config', 
-        'controller', 
-        # 'dmdc', 
-        'edmdc',
-        'pid_kmpc_gain.yaml'
-    )
-
-    ff_pi_lqt_gain = os.path.join(
-        get_package_share_directory('xplorer_mini_sysid'),
-        'config', 
-        'controller', 
-        # 'dmdc', 
-        'edmdc',
-        'ff_pi_lqt_gain.yaml'
-    )
     
-    ff_pi_kmpc_gain = os.path.join(
+    gain = os.path.join(
         get_package_share_directory('xplorer_mini_sysid'),
         'config', 
         'controller', 
-        # 'dmdc', 
-        'edmdc',
-        'ff_pi_kmpc_gain.yaml'
+        model,
+        'constrained' if constrained else 'unconstrained',
+        'with_preview' if use_preview else 'without_preview',
+        f'{pose_controller}_{vel_controller}_gain.yaml'
     )
 
     control_node = Node(
@@ -62,7 +46,7 @@ def generate_launch_description():
         name='kmpc_node',
         output='screen',
         emulate_tty=True,
-        parameters=[{ff_pi_kmpc_gain}, {'use_sim_time': True}]
+        parameters=[gain, auv_param , {'use_sim_time': True}]
     )
 
     return LaunchDescription([
